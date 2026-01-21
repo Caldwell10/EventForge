da93db93036ee3ed0aca48ca242ed9cd67bbd20a
Caldwell Wachira <143375845+Caldwell10@users.noreply.github.com>
2026-01-22 00:08:02 +0300

    Update auth.py

---
+++ app/auth.py
@@ -0,0 +1,30 @@
+import bcrypt
+from services import hash_password
+from fastapi.security import OAuth2AuthorizationCodeBearer
+from fastapi import HTTPException
+from jose import jwt, JWTError
+from datetime import datetime, timedelta, timezone
+from pydantic_settings import BaseSettings
+from app.config import Settings
+
+settings = Settings()
+
+oauth2_scheme = OAuth2AuthorizationCodeBearer(tokenUrl="token")
+
+def verify_password(plain, hashed) -> None:
+    hashed = hash_password(hashed)
+    bcrypt.checkpw(plain.encode("utf-8"), hashed.encode("utf-8"))
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy() 
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode.update({"exp": expire}) # add JWT expiration claim into payload
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
+
+def decode_token(token: str):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithm = [settings.ALGORITHM])
+        return payload
+    except JWTError:
+        raise HTTPException(status_code=401, detail = "Invalid token")
